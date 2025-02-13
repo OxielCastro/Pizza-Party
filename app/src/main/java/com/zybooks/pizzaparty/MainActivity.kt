e7d94bfb4e1e68d5c8f101548b6f3130c6ad8185
Oxiel <castrobadilloo27@hanover,edu>
2025-02-13 11:00:06 -0500

    after spinner

--- app/src/main/java/com/zybooks/pizzaparty/MainActivity.kt
+++ app/src/main/java/com/zybooks/pizzaparty/MainActivity.kt
@@ -2,8 +2,11 @@ package com.zybooks.pizzaparty
 
 import android.os.Bundle
 import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.EditText
 import android.widget.RadioGroup
+import android.widget.Spinner
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -15,14 +18,27 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var numAttendEditText: EditText
     private lateinit var numPizzasTextView: TextView
-    private lateinit var howHungryRadioGroup: RadioGroup
+    private lateinit var howHungrySpinner: Spinner
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         numAttendEditText = findViewById(R.id.num_attend_edit_text)
         numPizzasTextView = findViewById(R.id.num_pizzas_text_view)
-        howHungryRadioGroup = findViewById(R.id.hungry_radio_group)
+        howHungrySpinner = findViewById(R.id.hungry_spinner)
+
+        val hungerLevels = arrayOf("Craving","Famished","Ravenous")
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, hungerLevels )
+        howHungrySpinner.adapter = adapter
+
+        howHungrySpinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                numPizzasTextView.text = ""
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {}
+        }
+
     }
 
     fun calculateClick(view: View) {
@@ -34,9 +50,9 @@ class MainActivity : AppCompatActivity() {
         val numAttend = numAttendStr.toIntOrNull() ?: 0
 
         // Get hunger level selection
-        val hungerLevel = when (howHungryRadioGroup.checkedRadioButtonId) {
-            R.id.light_radio_button -> PizzaCalculator.HungerLevel.LIGHT
-            R.id.medium_radio_button -> PizzaCalculator.HungerLevel.MEDIUM
+        val hungerLevel = when (howHungrySpinner.selectedItemPosition) {
+            0 -> PizzaCalculator.HungerLevel.LIGHT
+            1 -> PizzaCalculator.HungerLevel.MEDIUM
             else -> PizzaCalculator.HungerLevel.RAVENOUS
         }
 
